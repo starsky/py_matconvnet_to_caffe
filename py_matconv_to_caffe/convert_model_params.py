819c8020c1e0f7b8354fb660c6ef8ab37cc250fc
Michal Koperski <michal.koperski@inria.fr>
2017-03-22 15:45:36 +0100

    Inferinging layers execution order.

--- py_matconv_to_caffe/convert_model_params.py
+++ py_matconv_to_caffe/convert_model_params.py
@@ -2,6 +2,9 @@ from caffe import layers as L
 import caffe
 from layers import layer_factory
 from utils import get_values_for_multi_keys
+import numpy as np
+from collections import defaultdict
+from _collections import deque
 """
 This module implements functionality which will convert architecture of the net from matconvnet to
 caffe. Including all parameters - number of layers, type of layers, layers params (eg. kernel size,
@@ -28,7 +31,19 @@ def convert_net(matconv_net):
     # dictionary with layer name as key and dict as a value. Dict contains lr_mult and decay_mult
     # which values are obtained from matconv_net.params
     lr_params_dic = _create_lr_params_dic(matconv_net.params)
-    for mcn_layer in matconv_net.layers:
+
+
+    layers_by_inputs = defaultdict(list)
+    for layer_id, l in enumerate(matconv_net.layers):
+        if hasattr(l.inputs, 'shape'):
+            for i in l.inputs:
+                layers_by_inputs[i].append((layer_id, l))
+        else:
+            layers_by_inputs[l.inputs].append((layer_id, l))
+    # layers_by_inputs = dict(layers_by_inputs)
+
+    execute_order = dfs(layers_by_inputs, (0, matconv_net.layers[0]))
+    for _, mcn_layer in execute_order:
         # searching for input layer based on input/output variable names
         bottom = get_values_for_multi_keys(layers_dic, mcn_layer.inputs)
         layer_name = mcn_layer.name
@@ -39,6 +54,23 @@ def convert_net(matconv_net):
             n.__setattr__('%s_INTER%02d' % (layer_name, i), t)
         n.__setattr__(layer_name, top[-1])
     return n
+
+
+def dfs(layers_by_inputs, start):
+    path = []
+    queue = deque()
+    queue.append(start)
+    visited = {}
+    while len(queue) > 0:
+        curr = queue.pop()
+        path.append(curr)
+        visited[curr[1].outputs] = True
+        for next_layer_id, next_layer in layers_by_inputs[curr[1].outputs]:
+            inputs = [next_layer.inputs] if not hasattr(next_layer.inputs, 'shape') else next_layer.inputs
+            is_ready = None not in [visited.get(inp) for inp in inputs]
+            if is_ready:
+                queue.append((next_layer_id, next_layer))
+    return path
 
 
 def _create_lr_params_dic(matconv_params_list):
